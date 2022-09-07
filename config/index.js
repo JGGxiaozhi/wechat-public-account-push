e9f2ffb0a0f5604610d5708020f8a263381132f1
JGGxiaozhi <104511502+JGGxiaozhi@users.noreply.github.com>
2022-09-07 08:44:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,12 +15,12 @@ export const config = {
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
+   * [o0Wid6eoapdLQvi_XaP5DgDTuQdw
    *  {
    *    // 想要发送的人的名字
    *    name: "老婆0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "o0Wid6eoapdLQvi_XaP5DgDTuQdw",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -33,22 +33,22 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "o0Wid6eoapdLQvi_XaP5DgDTuQdw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "2blVhTaWHa9JBiRmp0KmEo-B3LX_y0A8kYN8xQbTwjs",
       // 所在省份
-      province: "广东",
+      province: "四川",
       // 所在城市
-      city: "肇庆",
+      city: "成都",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
     {
       name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
+      id: "2blVhTaWHa9JBiRmp0KmEo-B3LX_y0A8kYN8xQbTwjs",
+      useTemplateId: "2blVhTaWHa9JBiRmp0KmEo-B3LX_y0A8kYN8xQbTwjs",
+      province: "四川",
+      city: "成都",
       openUrl: "https://wangxinleo.cn"
     }, 
     {
@@ -74,7 +74,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "2blVhTaWHa9JBiRmp0KmEo-B3LX_y0A8kYN8xQbTwjs",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -82,7 +82,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "o0Wid6aZPBE-CzIDimQzBh12p7ng",
     }, 
   ],
     
